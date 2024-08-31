aab86b3f926a2ba07d92a56abb780c3e36a757e3
Mohdsahil <sahils110043@gamil.com>
2024-08-31 11:37:59 +0530

    Start fronend

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'React App',
+            script: 'serve',
+            args: '-s build -l 3000', // Serve on port 3000
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
+};
