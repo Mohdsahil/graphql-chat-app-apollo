45253d9807fb1c9827fdd9918198ee858b91b987
Mohdsahil <sahils110043@gamil.com>
2024-08-31 11:41:38 +0530

    Change port

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'React App',
             script: 'serve',
-            args: '-s build -l 3000', // Serve on port 3000
+            args: '-s build -l 8080', // Serve on port 3000
             env: {
                 NODE_ENV: 'development',
             },
